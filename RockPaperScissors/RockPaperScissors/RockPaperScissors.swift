c59012540f871d70d0801d5e123a6d57714c4cab
CevatUygur <cevatuygur@gmail.com>
2022-03-27 00:46:42 +0300

    RockPaperScissors added

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by CEVAT UYGUR on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
