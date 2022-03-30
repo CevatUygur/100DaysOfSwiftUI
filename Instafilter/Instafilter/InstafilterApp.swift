51297c1181caca7d71745d3b6d639be0d4aa2f12
CevatUygur <cevatuygur@gmail.com>
2022-03-30 17:53:34 +0300

    First Commit

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by CEVAT UYGUR on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
