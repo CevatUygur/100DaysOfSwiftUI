009f9023c0a868d4b616129dac5938205c0b2872
CevatUygur <cevatuygur@gmail.com>
2022-04-01 20:16:27 +0300

    Map marker updated

--- BucketList/BucketList/Location.swift
+++ BucketList/BucketList/Location.swift
@@ -6,11 +6,22 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct Location: Identifiable, Codable, Equatable {
     let id: UUID
     var name: String
     var description: String
-    var latitude: Double
-    var longitude: Double
+    let latitude: Double
+    let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Queen Elizabeth lives with her dorgies", latitude: 51.501, longitude: -0.141)
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
