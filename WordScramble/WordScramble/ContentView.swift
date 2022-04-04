acc4ada42124779343eb6ed3c80f88290e13d15b
CevatUygur <cevatuygur@gmail.com>
2022-04-04 23:04:18 +0300

    Accessibility Voice Over feature added

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -32,6 +32,9 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 }
             }
