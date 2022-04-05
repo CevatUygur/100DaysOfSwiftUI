d32e2deae3aaa3838397aaad2028298af219eb60
CevatUygur <cevatuygur@gmail.com>
2022-04-05 20:41:09 +0300

    First Commit

---
+++ PhotoList/PhotoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoList
+//
+//  Created by CEVAT UYGUR on 5.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("PhotoList")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoList/PhotoList/PhotoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoListApp.swift
+//  PhotoList
+//
+//  Created by CEVAT UYGUR on 5.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
