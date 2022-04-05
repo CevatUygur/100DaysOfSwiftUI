bb219fa0b65d765e6c31964045dcc37184a1b6b8
CevatUygur <cevatuygur@gmail.com>
2022-04-05 21:04:43 +0300

    First Commit

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by CEVAT UYGUR on 5.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
