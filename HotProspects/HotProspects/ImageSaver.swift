05bcb54233da330a6e200ab4579d2c6efd51c059
CevatUygur <cevatuygur@gmail.com>
2022-04-09 00:02:45 +0300

    Context menu added to QR code

---
+++ HotProspects/HotProspects/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  HotProspects
+//
+//  Created by CEVAT UYGUR on 8.04.2022.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
