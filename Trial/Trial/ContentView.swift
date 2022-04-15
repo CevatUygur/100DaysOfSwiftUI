6dc5f675672226f1cdb5dba457899e60baf8490c
CevatUygur <cevatuygur@gmail.com>
2022-04-16 01:23:30 +0300

    Stack of cards added

---
+++ Trial/Trial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trial
+//
+//  Created by CEVAT UYGUR on 16.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trial/Trial/TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialApp.swift
+//  Trial
+//
+//  Created by CEVAT UYGUR on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
